d2344662a5f8a2328288312cac1364e4127a6814
rohit29032005 <rohit.lalbahadur2023@vitstudent.ac.in>
2025-05-22 23:57:03 +0530

    issue solved

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ const firebaseConfig = {
   storageBucket: "shakealert-908d0.appspot.com",
   messagingSenderId: "202678351500",
   appId: "1:202678351500:web:f442a6f1354dba4b746da9",
-  measurementId: "G-L3M6X4QG2E"
+  measurementId: "G-L3M6X4QG2E",
+  databaseURL: "https://shakealert-908d0-default-rtdb.firebaseio.com/"
 };
 
 // Init Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-// Push function
+// Send to Firebase
 function sendMotionData(x, y, z) {
   const motionRef = ref(database, 'motions/');
   const newMotionRef = push(motionRef);
@@ -28,11 +29,20 @@ function sendMotionData(x, y, z) {
   });
 }
 
-// Device motion listener
-window.addEventListener("devicemotion", function(event) {
-  const x = event.acceleration.x;
-  const y = event.acceleration.y;
-  const z = event.acceleration.z;
+// Start after DOM fully loaded
+window.addEventListener("load", () => {
+  window.addEventListener("devicemotion", function(event) {
+    const acc = event.accelerationIncludingGravity;
 
-  sendMotionData(x, y, z); // yaha data firebase me jaayega
+    if (acc) {
+      const x = acc.x || 0;
+      const y = acc.y || 0;
+      const z = acc.z || 0;
+
+      console.log(`📲 Motion: X=${x}, Y=${y}, Z=${z}`);
+      sendMotionData(x, y, z);
+    } else {
+      console.log("No motion data received.");
+    }
+  });
 });
